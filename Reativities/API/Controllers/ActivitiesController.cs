3dae7e5a82bd9033778421682451e9c497536495
Pedro Perdido <pedro3692@gmail.com>
2020-02-14 00:10:42 +0000

    cleanup and refactored client to use react hooks

---
+++ Reativities/API/Controllers/ActivitiesController.cs
@@ -0,0 +1,72 @@
+﻿using Application.Activities;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ActivitiesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<List<Activity>>>> List(CancellationToken cancellationToken)
+        {
+            var values = await _mediator.Send(new List.Query(), cancellationToken);
+            return Ok(values);
+        }
+
+        // GET api/activities/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> Details(Guid id)
+        {
+            var activity = await _mediator.Send(new Details.Query { Id = id });
+
+            if (activity != null)
+            {
+                return Ok(activity);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        // POST api/activities
+        [HttpPost]
+        public async Task<ActionResult<Activity>> Create(Create.Command command)
+        {
+            var Id = await _mediator.Send(command);
+
+            return Ok(Id);
+        }
+
+        // PUT api/activities/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+        {
+            command.Id = id;
+
+            return Ok(await _mediator.Send(command));
+        }
+
+        // DELETE api/activities/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Delete(Guid id)
+        {
+            return Ok(await _mediator.Send(new Delete.Command { Id = id }));
+        }
+    }
+}
---
+++ Reativities/Application/Activities/Create.cs
@@ -0,0 +1,57 @@
+﻿using Domain;
+using MediatR;
+using Persistence;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class Create
+    {
+        public class Command : IRequest<Guid>
+        {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string Category { get; set; }
+            public DateTime Date { get; set; }
+            public string City { get; set; }
+            public string Venue { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Guid>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Guid> Handle(Command request, CancellationToken cancellationToken)
+            {
+
+                var activity = new Activity
+                {
+                    Title = request.Title,
+                    Description = request.Description,
+                    Category = request.Category,
+                    Date = request.Date,
+                    Venue = request.Venue
+                };
+
+
+                _context.Activities.Add(activity);
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success)
+                {
+                    return activity.Id;
+                }
+
+                throw new Exception("Problem saving changes");         
+            }
+        }
+    }
+}
